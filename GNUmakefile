3b5bacdfe2d71345829f807bd545866ad7bc70d2
Olly Betts <olly@survex.com>
2017-09-19 14:02:34 +1200

    Rework autogen.sh to use bootstrap
    A few workarounds are needed for the version of bootstrap from
    v1.3.4 (at least to avoid needless additional work during the build):
    
    * this old version wants to build SWIG unless xapian-bindings is
      disabled via the existence of xapian-bindings/.nobootstrap (rather
      than via listing of modules on the command line)
    
    * we also need swig/.nobootstrap to stop a git clone of the SWIG repo
    
    * we create directories xapian-letor/common and
      xapian-applications/omega/common to stop bootstrap pointlessly hooking
      those up
    
    These are all already fixed in upstream git, but the fixes aren't
    trivial to cherry pick.

---
+++ GNUmakefile
@@ -0,0 +1,7 @@
+distcheck:
+	$(MAKE) -C xapian-core $@ && cp xapian-core/xapian*-core*.tar.xz .
+
+%:
+	$(MAKE) -C xapian-core $@
+
+.PHONY: distcheck
--- xapian-core/autogen.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-test -n "$srcdir" || srcdir=`dirname "$0"`
-test -n "$srcdir" || srcdir=.
-
-"$srcdir/preautoreconf"
-autoreconf -fiv
-test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
